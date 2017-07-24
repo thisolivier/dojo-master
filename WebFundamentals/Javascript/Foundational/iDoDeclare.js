3fa4bd377a6a23fe7e9415b793f9bfff8811d203
Olivier Butler <olivier.butler@gmail.com>
2017-07-24 08:33:34 -0700

    Adding console logging experiment

---
+++ WebFundamentals/Javascript/Foundational/iDoDeclare.js
@@ -0,0 +1,21 @@
+var num1 = 10/0,
+    num2 = 42,
+    num3 = 8.7,
+    num4 = -900060321,
+    string1 = "Hello strings, want cheese?",
+    string2 = "Where did all the lost boys go?",
+    string3 = "Do you like cheese?",
+    bool1 = true,
+    bool2 = false,
+    nope;
+
+console.log("Number one is " + num1);
+console.log("Number two is " + num2);
+console.log("Number three is " + num3);
+console.log("Number four is " + num4);
+console.log("Here's a string... " + string1);
+console.log("And another string..." + string2);
+console.log("The final string..." + string3);
+console.log("I like chocolate..." + bool1);
+console.log("I like beets..." + bool2);
+console.log("On god, what happened Jimmy?! " + nope);
