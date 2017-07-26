f62713b7b2f69954a8eae0032aa6bc3a91978091
Olivier Butler <olivier.butler@gmail.com>
2017-07-26 11:02:00 -0700

    Added contact card work

--- WebFundamentals/jQuery v2/ContactCard/script.js
+++ WebFundamentals/jQuery v2/ContactCard/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
     $('form').submit(function(event){
         event.preventDefault();
-        console.log($('form').serialize());
+        var formData = $('form').serialize().split("&");
+        var formObj = {};
+        for (var idx = 0; idx < formData.length; idx++) {
+            var itemData = formData[idx].split("=");
+            formObj[itemData[0]] = itemData[1];
+        }
+        console.log(formObj);
+        makeCard(formObj);
     })
+
+    
 });
+
+function makeCard(person) {
+    var newCard = $('.card').first().clone();
+    $(newCard).children('h2').text(person.first_name + " " + person.last_name);
+    $(newCard).children('.back').text(person.description);
+    $('.cards').append(newCard);
+}
