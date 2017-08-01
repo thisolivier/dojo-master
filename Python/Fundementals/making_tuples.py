5d3c77dfcfaf9d12b99821012dfda4540094edce
Olivier Butler <olivier.butler@gmail.com>
2017-07-31 21:40:56 -0700

    Making tuples finished

---
+++ Python/Fundementals/making_tuples.py
@@ -0,0 +1,10 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+def make_tuples(dict):
+    return dict.items()
+
+print make_tuples(my_dict)
