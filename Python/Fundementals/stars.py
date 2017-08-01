c418709707fab4ea1428df971da453f98023b707
Olivier Butler <olivier.butler@gmail.com>
2017-07-31 21:15:35 -0700

    Stars part 2 completed

--- Python/Fundementals/stars.py
+++ Python/Fundementals/stars.py
@@ -1,7 +1,13 @@
 def print_magic(list):
-    for noStars in list:
-        stars = "*"
+    for things in list:
+        if str(type(things)).find('str') >= 0:
+            noStars = len(things)
+            stars = things[0].lower()
+        else :
+            noStars = things
+            stars = "*"
+
         print stars * noStars
 
-x = [4, 6, 1, 3, 5, 7, 25]
+x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
 print_magic(list = x)
