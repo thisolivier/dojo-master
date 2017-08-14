d09e7ed35230d0865ccc13297e8342221b3ffda9
Olivier Butler <olivier.butler@gmail.com>
2017-08-14 00:17:34 -0700

    Finished dojo_ninjas

--- Python/Django/dojo_ninjas/apps/ninjas_app/models.py
+++ Python/Django/dojo_ninjas/apps/ninjas_app/models.py
@@ -7,6 +7,7 @@ class Dojo(models.Model):
     name = models.CharField(max_length = 255)
     city = models.CharField(max_length = 255)
     state = models.CharField(max_length = 255)
+    description = models.TextField()
 
 class Ninja(models.Model):
     first_name = models.CharField(max_length = 255)
