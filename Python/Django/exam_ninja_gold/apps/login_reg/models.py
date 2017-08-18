d19228cd695998aa9e1d63439a16a5ef35cab855
Olivier Butler <olivier.butler@gmail.com>
2017-08-18 10:30:46 -0700

    Model works

--- Python/Django/exam_ninja_gold/apps/login_reg/models.py
+++ Python/Django/exam_ninja_gold/apps/login_reg/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email = models.CharField(max_length=255),
-    password = models.CharField(max_length=255),
-    gold = models.IntegerField(default="0"),
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    gold = models.IntegerField(default="0")
     log = models.TextField(max_length=2000, default="")
