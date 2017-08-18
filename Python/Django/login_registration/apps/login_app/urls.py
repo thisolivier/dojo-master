54e14b1b2e26c56fa37b0f2d1e4f93982545d989
Olivier Butler <olivier.butler@gmail.com>
2017-08-18 09:48:32 -0700

    Working

--- Python/Django/login_registration/apps/login_app/urls.py
+++ Python/Django/login_registration/apps/login_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.login_page),
-    url(r'register$', views.register),
-    url(r'login$', views.login)
+    url(r'^/register', views.register_page),
+    url(r'^/register/do', views.register),
+    url(r'^/do', views.login)
 ]
