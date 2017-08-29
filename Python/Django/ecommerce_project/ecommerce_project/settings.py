16edfd465abc41a9beedb5097bccde82bb5d19b0
Olivier Butler <olivier.butler@gmail.com>
2017-08-29 12:43:12 -0700

    Catching minor changes, beginning work on flask/dojo_survey validation

--- Python/Django/ecommerce_project/ecommerce_project/settings.py
+++ Python/Django/ecommerce_project/ecommerce_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'social_django',
     'apps.portal_app',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -99,6 +100,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',
+    'social_core.backends.google.GoogleOpenId',
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.google.GoogleOAuth',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.yahoo.YahooOpenId',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
 
 # Internationalization
