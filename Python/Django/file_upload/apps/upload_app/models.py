bdbadfac493ac8f7acc943d50f2f1b2365b49224
Olivier Butler <olivier.butler@gmail.com>
2017-08-31 18:56:25 -0700

    File uploads now working fully and saving in database. Using model forms to simplify process

--- Python/Django/file_upload/apps/upload_app/models.py
+++ Python/Django/file_upload/apps/upload_app/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    category = models.CharField(max_length=50)
+    file = models.FileField(upload_to='')
